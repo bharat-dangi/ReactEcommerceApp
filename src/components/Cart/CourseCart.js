9db89f87fb1265262f7c3cab44ae5dc52a429591
bharatdangi2074 <bharatdangi2074@gmail.com>
2021-03-16 01:19:24 +0545

    category filter added

--- src/components/Cart/CourseCart.js
+++ src/components/Cart/CourseCart.js
@@ -2,13 +2,42 @@ import React, { Component } from "react"
 import Heading from "../Reusable/Heading"
 import Img from "gatsby-image"
 
+const getCategory = items => {
+  let holdItems = items.map(items => {
+    return items.node.category
+  })
+  let holdCategories = new Set(holdItems)
+  let categories = Array.from(holdCategories)
+  categories = ["all", ...categories]
+  return categories
+}
+
 export default class CourseCart extends Component {
   constructor(props) {
     super(props)
     this.state = {
       courses: props.courses.edges,
       myCourses: props.courses.edges,
+      myCategories: getCategory(props.courses.edges),
     }
+  }
+
+  catyClicked=category=>{
+    let keepItSafe=[...this.state.courses]
+
+if(category==='all'){
+  this.setState(()=>{
+    return {myCourses:keepItSafe}
+  })
+
+}
+else{
+  let holder=keepItSafe.filter(({node})=>node.category===category)
+  this.setState(()=>{
+    return {myCourses:holder}
+  })
+}
+
   }
 
   render() {
@@ -17,6 +46,24 @@ export default class CourseCart extends Component {
       <section className="py-5">
         <div className="container">
           <Heading title="Courses" />
+          <div className="row my-3">
+            <div className="col-10 mx-auto text-center">
+              {this.state.myCategories.map((category, index) => {
+                return (
+                  <button
+                    type="button"
+                    className="btn btn-info m-3 px-3"
+                    key={index}
+                    onClick={()=>{
+                      this.catyClicked(category)
+                    }}
+                  >
+                    {category}
+                  </button>
+                )
+              })}
+            </div>
+          </div>
           <div className="row">
             {this.state.myCourses.map(({ node }) => {
               return (
